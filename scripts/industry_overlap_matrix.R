d7888bef140a8bbbabd4ad75e78212d5ec5ffe2a
Jackson Stockbridge <s2997201@staff.ad.griffith.edu.au>
2024-08-02 14:59:25 +1000

    script changes
    output upload

--- scripts/industry_overlap_matrix.R
+++ scripts/industry_overlap_matrix.R
@@ -18,15 +18,17 @@ sf_use_s2(F)
 # Should we be using an Australian specific equal area projection?
 
 # Import project ocean uses data
-no.uses_sf_prj_all <- readRDS(here('output_data', 'ocean_uses', 'no_uses_sf_prj_all'))
+no_uses_sf_prj_all <- readRDS(here('output_data/ocean_uses/no_uses_sf_prj_all'))
 
-list_sf_trans <- lapply(no.uses_sf_prj_all, function(x) st_transform(x, "+proj=aea +lat_0=0 +lon_0=132 +lat_1=-18 +lat_2=-36 +x_0=0 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs
+# Project
+list_sf_trans <- lapply(no_uses_sf_prj_all, function(x) st_transform(x, "+proj=aea +lat_0=0 +lon_0=132 +lat_1=-18 +lat_2=-36 +x_0=0 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs
 "))
 
+# Make geoms valid
 list_sf_trans2 <- lapply(list_sf_trans, function(x) st_make_valid(x))
 
 # Import EEZ boundary
-eez_boundary <- st_read(here('output_data', 'ocean_uses', 'eez_boundary.gpkg'))
+eez_boundary <- st_read(here('output_data/ocean_uses/eez_boundary.gpkg'))
 
 # Project eez
 eez_boundary <- st_transform(eez_boundary, "+proj=aea +lat_0=0 +lon_0=132 +lat_1=-18 +lat_2=-36 +x_0=0 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs
@@ -34,14 +36,17 @@ eez_boundary <- st_transform(eez_boundary, "+proj=aea +lat_0=0 +lon_0=132 +lat_1
   st_make_valid(.)
 
 # Crop all to eez
-no.uses_sf_prj_crop <- lapply(list_sf_trans2, st_crop, eez_boundary)
+no_uses_sf_prj_crop <- lapply(list_sf_trans2, st_crop, eez_boundary)
 
-# Rasterize and create dataframes
 
+## Rasterize and create dataframes
+
+# Create template raster grid
 temp_rast<-raster(eez_boundary, res = 1000) #determine resolution to use
 
-#x<-no.uses_sf_prj_crop[[1]]
+#x<-no_uses_sf_prj_crop[[1]]
 
+# Create ocean uses rasters and dataframes
 rast_uses<-function(x){
   use_rast<-fasterize(x, temp_rast, field = NULL, fun = "last", background = NA)
   use_df<-as_tibble(use_rast, xy = TRUE)
@@ -50,21 +55,17 @@ rast_uses<-function(x){
   writeRaster(use_rast, paste0(here("output_data", 'ocean_uses'), name, "_ras.tif"), overwrite = T)
   write_csv(use_df, paste0(here("output_data", "ocean_uses"), name, "_df.csv"))
 }
-
-
-pbmclapply(no.uses_sf_prj_crop, rast_uses, mc.cores = length(no.uses_sf_prj_crop), mc.style = "ETA")
+pbmclapply(no_uses_sf_prj_crop, rast_uses, mc.cores = length(no_uses_sf_prj_crop), mc.style = "ETA")
 
 # Read in dataframes and group together
-
 use_file_list<-list.files(here(""))
-
 
 
 ## Loop through the ocean uses list and generate 
 ## a matrix with number of overlap occurrences
 
 # Length of list
-n <- length(no.uses_sf_prj_crop)
+n <- length(no_uses_sf_prj_crop)
 
 # Get all combos of list elements and exclude 1-1, 2-2, 3-3 etc.
 combodf <- expand.grid(x = 1:n, y = 1:n) %>% 
@@ -74,17 +75,13 @@ combodf <- expand.grid(x = 1:n, y = 1:n) %>%
 tmp <- list()
 
 
-### Having computer memory issues
-### Will run the loops in chunks and combine later
-
-
-
+### Computer performance may require running this in chunks and combine later
 
 # Loop to find intersects
  for(i in 1:nrow(combodf)){
    combo <- combodf[i,] # Create index (where will the loop look for values?)
-   first <- no.uses_sf_prj_crop[[combo$x]] # Indicate where to find first industry
-   second <- no.uses_sf_prj_crop[[combo$y]] # Indicate where to find second industry
+   first <- no_uses_sf_prj_crop[[combo$x]] # Indicate where to find first industry
+   second <- no_uses_sf_prj_crop[[combo$y]] # Indicate where to find second industry
    int <- st_overlaps(first, second, sparse = F) # Check for overlap = TRUE
    # Sum elements where overlap = TRUE - WHAT DOES ELEMENTS MEAN?
    count <- sum(int, na.rm=TRUE)
@@ -98,8 +95,8 @@ results <- do.call(c, tmp)
 # Add results to dataframe of combinations
 combodf$num_overlaps <- results
 
-# Save
-write_csv(combodf, here('output_data', 'ocean_uses', 'combodf.csv'))
+# # Save
+# write_csv(combodf, here('output_data/ocean_uses/combodf.csv'))
 
 
 ### Following is for Bass Strait and may need to be changed for Australia
@@ -143,4 +140,4 @@ matrix_table <- matrix_table %>%
   row_to_names(row_number = 1)
 
 # Save
-write_csv(matrix_table, here('output_data', 'ocean_uses', 'matrix_table.csv'))
+write_csv(matrix_table, here('output_data/ocean_uses/industry_overlap_AUS.csv'))
